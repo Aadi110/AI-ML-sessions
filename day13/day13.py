d52997a55a380c607e8182e31db1b7f5ba3db7af
Aaditya Pangeni <pangeniaaditya@gmail.com>
2026-08-04 11:05:47 +0545

    day13 matplotlib cw

--- day13/day13.py
+++ day13/day13.py
@@ -1,3 +1,33 @@
 #matplotlib
 
+import matplotlib.pyplot as plt
+
+# x=[1,2,3,4]
+# y=[1,4,9,16]
+
+# plt.plot(x,y)
+# plt.show()
+
+''' Line Graph representation  '''
+# months=['jan','feb','mar','apr']
+# sales=[200,250,400,350]
+
+# plt.plot(sales,months, marker='o', color='g', linestyle='--')
+# plt.title('Monthly sales')
+# plt.xlabel('Sales')
+# plt.ylabel('Months')
+# plt.grid()
+# plt.show()
+
+
+products=['Laptop', 'Smartphone', 'Tablet', 'Headphones', 'Smartwatch']
+sales=[150, 300, 200, 100, 80]
+plt.bar(products, sales, color='skyblue', edgecolor='black', width=0.6)
+plt.title('Product Sales')
+plt.xlabel('Products')
+plt.ylabel('Number of Sales')
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.show()
+
+
 
