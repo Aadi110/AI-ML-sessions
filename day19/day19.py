b70a6927aa9921e5d05dc0e3520b03889d1fcd64
Aaditya Pangeni <pangeniaaditya@gmail.com>
2026-08-12 11:55:16 +0545

    house prediction

---
+++ day19/day19.py
@@ -0,0 +1,30 @@
+# House Price Prediction Model
+
+'''
+ 1. What will be the output of this system?
+- House price will be predicted
+- Selling Price
+- Market Value
+
+X={
+    Area,
+    Bedroom,
+    no of years,
+    distance from city center
+}
+
+y={
+    to predict === house price-> Numerical value, quantitative data => Regression Analysis, Linear Regression is used.
+}
+
+
+What is remaining? 
+
+- Train(80%) and Test(20%) => Split the data
+- Train the model
+- Test the model
+
+'''
+
+
+
